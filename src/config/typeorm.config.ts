b7be0bb95ef6bb1647e029ef3140fa875763c35b
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 00:52:34 +0330

    add TypeORM configuration for PostgreSQL database using ConfigService

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from "@nestjs/common";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class TypeOrmDbConfig implements TypeOrmOptionsFactory {
+    constructor(private configService: ConfigService){}
+    createTypeOrmOptions(connectionName?: string): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+        return {
+            type: "postgres",
+            database: this.configService.get("Db.database"),
+            port: this.configService.get("Db.port"),
+            host: this.configService.get("Db.host"),
+            username: this.configService.get("Db.username"),
+            password: this.configService.get("Db.password"),
+            synchronize: true
+        }
+    }
+}
