47404c7ff65a7e7ad93dcb7da1224868dd6ce37d
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 01:08:26 +0330

    refactor ConfigsCustomModule for improved formatting and consistency

--- src/modules/config/configs.module.ts
+++ src/modules/config/configs.module.ts
@@ -1,13 +1,13 @@
 import { ConfigModule } from "@nestjs/config";
-import {Module} from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { configurations } from "src/config/config";
 
 @Module({
-    imports: [ConfigModule.forRoot({
-        load: configurations,
-        isGlobal: true,
-    })]
+  imports: [
+    ConfigModule.forRoot({
+      load: configurations,
+      isGlobal: true,
+    }),
+  ],
 })
-export class ConfigsCustomModule {
-
-}
+export class ConfigsCustomModule {}
