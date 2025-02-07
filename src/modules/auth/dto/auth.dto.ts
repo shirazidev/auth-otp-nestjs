c44bb50e4be79c9d842749d016b625e66a1c7e33
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 03:19:05 +0330

    add DTOs for OTP handling with validation rules

---
+++ src/modules/auth/dto/auth.dto.ts
@@ -0,0 +1,30 @@
+import {
+  IsMobilePhone,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Length,
+} from "class-validator";
+
+export class SendOtpDto {
+  @IsMobilePhone(
+    "fa-IR",
+    {},
+    { message: "You can only enter Iranian phone number." }
+  )
+  @IsNotEmpty()
+  mobile: string;
+}
+export class CheckOtpDto {
+  @IsMobilePhone(
+    "fa-IR",
+    {},
+    { message: "You can only enter Iranian phone number." }
+  )
+  @IsNotEmpty()
+  mobile: string;
+  @IsString()
+  @Length(5, 5, { message: "the code should have 5 chars." })
+  @IsNotEmpty()
+  code: string;
+}
