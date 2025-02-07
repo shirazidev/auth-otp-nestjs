d36a9895ae78f9e44cf64c470dd2a4088aec65eb
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 01:08:07 +0330

    add user entity with fields for username, mobile, name, and timestamps

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity("user")
+export class UserEntity {
+    @PrimaryGeneratedColumn("increment")
+    id: number;
+    @Column({unique: true})
+    username: string;
+    @Column({unique: true})
+    mobile: string;
+    @Column({default: false})
+    mobile_verified: boolean;
+    @Column()
+    first_name: string;
+    @Column()
+    last_name: string;
+    @CreateDateColumn()
+    created_at: Date;
+    @UpdateDateColumn()
+    updated_at: Date;
+}
