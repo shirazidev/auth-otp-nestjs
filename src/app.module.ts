4372fe7d9896aa4ee35441bf685322d383db540b
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 00:56:48 +0330

    add user module with CRUD operations and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { ConfigsCustomModule } from "./modules/config/configs.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TypeOrmDbConfig } from "./config/typeorm.config";
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [ConfigsCustomModule, TypeOrmModule.forRootAsync({
     useClass: TypeOrmDbConfig,
     inject: [TypeOrmDbConfig]
-  })],
+  }), UserModule],
   controllers: [],
   providers: [TypeOrmDbConfig],
 })
