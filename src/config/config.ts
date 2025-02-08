b893a2ba1f2a355ed67e35dff4a7afad43d272cc
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 04:40:04 +0330

    add JWT configuration to the application settings

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,16 +4,21 @@ import { registerAs } from "@nestjs/config";
 export enum ConfigKeys {
   App = "App",
   Db = "Db",
+  Jwt = "Jwt",
 }
 const AppConfig = registerAs(ConfigKeys.App, () => ({
-    port: process.env.PORT || 3000,
-}))
+  port: process.env.PORT || 3000,
+}));
 const DbConfig = registerAs(ConfigKeys.Db, () => ({
-    port: process.env.DBPORT || 5432,
-    host: process.env.DBHOST || "localhost",
-    username: process.env.DBUSERNAME || "postgres",
-    password: process.env.DBPASSWORD || "123456",
-    database: process.env.DB || "auth-otp",
-}))
+  port: process.env.DBPORT || 5432,
+  host: process.env.DBHOST || "localhost",
+  username: process.env.DBUSERNAME || "postgres",
+  password: process.env.DBPASSWORD || "123456",
+  database: process.env.DB || "auth-otp",
+}));
+const JwtConfig = registerAs(ConfigKeys.Jwt, () => ({
+  accessTokenSecret: process.env.ACCESSTOKENJWT || "hard?@to@#guess",
+  refreshTokenSecret: process.env.REFRESHTOKENJWT || "hard?@to@#guess",
+}));
 
-export const configurations = [AppConfig, DbConfig]
+export const configurations = [AppConfig, DbConfig, JwtConfig];
