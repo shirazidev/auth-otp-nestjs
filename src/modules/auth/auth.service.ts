8c4b7dbefc4ede53caa183b07fadc66660173963
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 04:05:42 +0330

    enhance OTP validation logic in AuthService with error handling and mobile verification

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,11 @@
 import { SendOtpDto, CheckOtpDto } from "./dto/auth.dto";
-import { Inject, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { UserEntity } from "../user/entities/user.entity";
 import { Repository } from "typeorm";
@@ -25,13 +31,46 @@ export class AuthService {
       message: "sent code successfully!",
     };
   }
-  async checkOtp(CheckOtpDto) {}
+  async checkOtp(otpDto: CheckOtpDto) {
+    const { mobile, code } = otpDto;
+    const user = await this.userRepository.findOne({
+      where: { mobile },
+      relations: {
+        otp: true,
+      },
+      select: {
+        otp: {
+          code: true,
+          expires_in: true,
+          userId: true,
+        },
+      },
+    });
+    const now = new Date();
+    const otp = user?.otp;
+    if (!user || !otp)
+      throw new UnauthorizedException("we cannot authorize you");
+    if (code !== otp?.code)
+      throw new UnauthorizedException("your code is invalid");
+    if (otp?.expires_in < now)
+      throw new UnauthorizedException("your code is expired");
+    if (!user.mobile_verified) {
+      await this.userRepository.update({id: user.id}, {mobile_verified: true})
+    }
+    return {
+      message: "signed in successfully",
+    };
+  }
 
   async createOtpForUser(user: UserEntity) {
     const code: string = randomInt(10000, 99999).toString();
     const expires_in = new Date(new Date().getTime() + 1000 * 60 * 2);
     let otp = await this.otpRepository.findOneBy({ userId: user.id });
+
     if (otp) {
+      if (otp.expires_in > new Date()) {
+        throw new BadRequestException("try later, otp code not expired.");
+      }
       otp.code = code;
       otp.expires_in = expires_in;
     } else {
@@ -40,9 +79,9 @@ export class AuthService {
         code,
         expires_in,
       });
-      await this.otpRepository.save(otp);
       user.otpId = otp.id;
       await this.userRepository.save(user);
     }
+    await this.otpRepository.save(otp);
   }
 }
