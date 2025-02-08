9e9169f23e6b81447b2c23cfe608de70beba8373
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 04:05:31 +0330

    add TypeOrmModule imports for UserEntity and OtpEntity in AuthModule

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { UserEntity } from "../user/entities/user.entity";
+import { OtpEntity } from "../user/entities/otp.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity, OtpEntity])],
   controllers: [AuthController],
   providers: [AuthService],
 })
