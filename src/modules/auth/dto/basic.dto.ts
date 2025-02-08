dfcd0832cb6149f379859ca3fcf2d51e3da78030
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 22:40:47 +0330

    create basic dto for login and signup with email and password

---
+++ src/modules/auth/dto/basic.dto.ts
@@ -0,0 +1,57 @@
+import {
+  IsEmail,
+  IsMobilePhone,
+  IsNotEmpty,
+  IsString,
+  Length,
+} from "class-validator";
+
+export class SignupDto {
+  @IsString()
+  first_name: string;
+  @IsString()
+  last_name: string;
+  @IsString()
+  @IsEmail(
+    {
+      host_whitelist: [
+        "gmail.com",
+        "yahoo.com",
+        "hotmail.com",
+        "outlook.com",
+        "outlook.de",
+      ],
+    },
+    { message: "Email is not valid" },
+  )
+  email: string;
+  @IsMobilePhone(
+    "fa-IR",
+    {},
+    { message: "You can only enter Iranian phone number." },
+  )
+  @IsNotEmpty()
+  mobile: string;
+  @IsString()
+  @Length(8, 20, { message: "Password should be between 8 and 20 characters" })
+  password: string;
+  @IsString()
+  confirm_password: string;
+}
+export class LoginDto {
+  @IsEmail(
+    {
+      host_whitelist: [
+        "gmail.com",
+        "yahoo.com",
+        "hotmail.com",
+        "outlook.com",
+        "outlook.de",
+      ],
+    },
+    { message: "Email is not valid" },
+  )
+  email: string;
+  @IsString()
+  password: string;
+}
