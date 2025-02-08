894d7bb5b2d9eef5bb7ae27014f4a5f39506d87a
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 04:05:23 +0330

    add OtpEntity to UserModule imports

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserEntity } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { OtpEntity } from './entities/otp.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, OtpEntity])],
   controllers: [UserController],
   providers: [UserService],
 })
