c5f21d6e977592ff087dd8e5818533dc259adef6
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 04:40:13 +0330

    add JwtService to AuthModule providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthController } from "./auth.controller";
 import { UserEntity } from "../user/entities/user.entity";
 import { OtpEntity } from "../user/entities/otp.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { JwtService } from "@nestjs/jwt";
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity, OtpEntity])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtService],
 })
 export class AuthModule {}
