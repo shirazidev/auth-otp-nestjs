55442e97c9f7d7bc3c40308e3265f103333f6fac
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 22:18:55 +0330

    enhance UserEntity by adding email and password fields, and improving column definitions

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,25 +1,37 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { OtpEntity } from "./otp.entity";
 
 @Entity("user")
 export class UserEntity {
-    @PrimaryGeneratedColumn("increment")
-    id: number;
-    @Column({unique: true})
-    mobile: string;
-    @Column({default: false})
-    mobile_verified: boolean;
-    @Column({nullable: true})
-    first_name: string;
-    @Column({nullable: true})
-    last_name: string;
-    @CreateDateColumn()
-    created_at: Date;
-    @UpdateDateColumn()
-    updated_at: Date;
-    @Column({nullable: true})
-    otpId: number;
-    @OneToOne(()=> OtpEntity, otp => otp.user)
-    @JoinColumn({name: "otpId"})
-    otp: OtpEntity
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+  @Column({ unique: true, nullable: true })
+  mobile: string;
+  @Column({ default: false })
+  mobile_verified: boolean;
+  @Column({ nullable: true })
+  first_name: string;
+  @Column({ nullable: true })
+  last_name: string;
+  @Column({ unique: true, nullable: true })
+  email: string;
+  @Column({ nullable: true })
+  password: string;
+  @CreateDateColumn()
+  created_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
+  @Column({ nullable: true })
+  otpId: number;
+  @OneToOne(() => OtpEntity, (otp) => otp.user)
+  @JoinColumn({ name: "otpId" })
+  otp: OtpEntity;
 }
