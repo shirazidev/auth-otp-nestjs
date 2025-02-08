14e719da4074db5ad92701dee3fa971345276090
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 15:26:22 +0330

    import AuthModule into UserModule to enable authentication features

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,12 +1,14 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
-import { UserEntity } from './entities/user.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { OtpEntity } from './entities/otp.entity';
+import { AuthModule } from "./../auth/auth.module";
+import { Module } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { UserController } from "./user.controller";
+import { UserEntity } from "./entities/user.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { OtpEntity } from "./entities/otp.entity";
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity, OtpEntity])],
+  imports: [AuthModule, TypeOrmModule.forFeature([UserEntity, OtpEntity])],
   controllers: [UserController],
   providers: [UserService],
 })
