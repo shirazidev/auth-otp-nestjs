5061558690a99f58a85ff56954577cd405843ea3
AmirHossein Shirazi <ashzih2@gmail.com>
2025-02-08 04:05:46 +0330

    add OTP sending and validation endpoints in AuthController

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CheckOtpDto, SendOtpDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @Post('/send-otp')
+  async sendOtp(@Body() otpDto: SendOtpDto) {
+    return await this.authService.sendOtp(otpDto)
+  }
+  @Post('/check-otp')
+  async checkOtp(@Body() otpDto: CheckOtpDto) {
+    return await this.authService.checkOtp(otpDto)
+  }
 }
